9d4775937620bb925548dd2d87a122e0164cf8e4
Olli-Antti Kivilahti <kivilahtio@gmail.com>
2016-03-15 18:52:27 +0200

    Introducing the Swagger2 API

--- cee_vee_licious/lib/CeeVeeLicious.pm
+++ cee_vee_licious/lib/CeeVeeLicious.pm
@@ -4,7 +4,7 @@
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -13,33 +13,54 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 package CeeVeeLicious;
 use Mojo::Base 'Mojolicious';
+
+use File::Basename;
 
 use CeeVeeLicious::Assets;
 
 sub startup {
   my $self = shift;
-  my $app = $self->app;
-  $app->secrets(['No day like today!']);
+  $self->app->secrets(['No day like today!']);
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
-  $self->plugin('AssetPack');
-
-  CeeVeeLicious::Assets->introduce($app);
-
-  $self->_defineRoutes();
+  $self->setPlugins();
+  $self->defineRoutes();
 }
 
-sub _defineRoutes {
+sub setPlugins {
+  my ($self) = @_;
+  my $app = $self->app;
+  my $swaggerMainFile = $app->home->rel_file("/public/swagger/swagger.json");
+
+  $self->plugin('PODRenderer');
+  $self->plugin('AssetPack');
+  $self->plugin(Swagger2 => {url => $swaggerMainFile});
+
+  $self->minifySwagger($swaggerMainFile);
+  CeeVeeLicious::Assets->introduce($app);
+}
+
+sub defineRoutes {
   my ($self) = @_;
   my $r = $self->routes;
 
   # Normal route to controller
   $r->get('/')->to(controller => 'main', action => 'webapp');
   $r->get('/example')->to(controller => 'example', action => 'welcome');
+}
+
+sub minifySwagger {
+    my ($self, $swaggerMainFile) = @_;
+
+    my $swaggerPath = File::Basename::dirname($swaggerMainFile).'/';
+    my $pathToMinifier = $swaggerPath.'minifySwagger.pl';
+    my $pathToSwaggerJson = $swaggerPath.'swagger.json';
+    my $pathToSwaggerMinJson = $swaggerPath.'swagger.min.json';
+    my $output = `perl $pathToMinifier -s $pathToSwaggerJson -d $pathToSwaggerMinJson`;
+    if ($output) {
+        die $output;
+    }
 }
 
 1;
